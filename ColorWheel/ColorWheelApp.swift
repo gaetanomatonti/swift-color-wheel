36c829879f3617db3b402956a14b9bca9747ae1b
gaetanomatonti <gaetanomatonti@icloud.com>
2024-03-07 22:58:09 +0100

    Initial Commit

---
+++ ColorWheel/ColorWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorWheelApp.swift
+//  ColorWheel
+//
+//  Created by Gaetano Matonti on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
